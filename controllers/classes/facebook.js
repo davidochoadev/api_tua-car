562807240678d20e5b765c5d5ac3f5b782663db2
davideochoaa <davideochoaa@gmail.com>
2023-07-16 18:03:21 +0200

    🛠 Fix: Changed email, pass

--- controllers/classes/facebook.js
+++ controllers/classes/facebook.js
@@ -35,8 +35,9 @@ export default class Facebook{
       await page.evaluate((val) => email.value = val, this.email);
       await page.evaluate((val) => pass.value = val, this.password);
       await page.evaluate(selector => document.querySelector(selector).click(), 'input[value="Log In"],#loginbutton');
+      await page.screenshot({ path: 'location_milan_4.png' });
       await page.waitForNavigation({waitUntil: 'networkidle2'});
-      await page.screenshot({ path: 'location_milan_3.png' });
+      await page.screenshot({ path: 'location_milan_5.png' });
       console.log(chalk.bgGreen("Login Completed!"));
 /*       await page.goto(`https://www.facebook.com/marketplace/${location}/cars/`, { waitUntil: 'networkidle2' }); */
       console.log(`Searching on ${location}!`);
